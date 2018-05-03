e8d61e6d08f50afc0ac325df1fc75cd5f6846ee3
Alexander Gordeev <agordeevw@gmail.com>
2018-05-04 05:57:58 +0700

    Reimplemented core providers

--- lab2-Streams/include/StreamProviders.h
+++ lab2-Streams/include/StreamProviders.h
@@ -4,30 +4,43 @@
 #include <memory>
 #include <type_traits>
 #include <list>
+#include <stdexcept>
+#include <optional>
 
 namespace stream {
-
 namespace providers {
 
-template<class T>
-class StreamProvider
+class EmptyStreamException : public std::logic_error
 {
 public:
-  virtual ~StreamProvider() = default;
-  virtual bool advance() = 0;
-  virtual std::shared_ptr<T> get() = 0;
+  EmptyStreamException() : 
+  std::logic_error("ERROR: Empty stream") {};
 };
 
-template<class T, class Iter>
-class Iterator : public StreamProvider<T>
+template <class Derived>
+class ProviderInterface
 {
 public:
-  Iterator(Iter begin, Iter end) :
-    current(begin), end(end) {}
+  using value_type = typename Derived::value_type;
 
-  virtual ~Iterator() = default;
+  bool Advance() { return AdvanceImpl(); }
+  std::shared_ptr<value_type> Get() { return GetImpl(); }
+};
 
-  bool advance() override {
+namespace core {
+
+template <class IteratorType>
+class Iterator : public ProviderInterface<Iterator<IteratorType>>
+{
+public:
+  using value_type = typename IteratorType::value_type;
+
+  Iterator(IteratorType begin, IteratorType end) :
+  current(begin), end(end)
+  {}
+
+private:
+  bool AdvanceImpl() {
     if (first) {
       first = false;
       return current != end;
@@ -36,8 +49,8 @@ public:
     return current != end;
   }
 
-  std::shared_ptr<T> get() override {
-    return std::make_shared<T>(std::move(*current));
+  std::shared_ptr<value_type> GetImpl() {
+    return std::make_shared<value_type>(*current);
   }
 
   bool first = true;
@@ -45,179 +58,82 @@ public:
   Iter end;
 };
 
-template<class Container, class InternalStreamProvider = 
-    providers::Iterator<
-    typename Container::value_type,
-    typename Container::iterator>>
-class ContainerOwner : public StreamProvider<typename Container::value_type>
+template <class GeneratorType>
+class Generator : public ProviderInterface<Generator<GeneratorType>>
 {
 public:
-  ContainerOwner(Container&& cont) :
-    container(std::move(cont)),
-    source(std::make_unique<InternalStreamProvider>(
-      container.begin(), container.end())) {}
+  using value_type = std::invoke_result_t<GeneratorType>;
 
-  virtual ~ContainerOwner() = default;
-
-  bool advance() override {
-    return source->advance();
-  }
-
-  std::shared_ptr<typename Container::value_type> get() override {
-    return source->get();
-  }
+  Generator(GeneratorType&& generator) :
+  generator(std::forward<GeneratorType>(generator))
+  {}
 
 private:
-  Container container;
-  std::unique_ptr<InternalStreamProvider> source;
-};
-
-template<class Generator>
-class Generate : public StreamProvider<std::result_of_t<Generator()>>
-{
-using T = std::result_of_t<Generator()>;
-
-public:
-  Generate(Generator&& generator):
-    generator(std::forward<Generator>(generator)) {}
-
-  virtual ~Generate() = default;
-
-  bool advance() override {
+  bool AdvanceImpl() {
     current = std::make_shared<T>(generator());
     return true;
   }
 
-  std::shared_ptr<T> get() override {
+  std::shared_ptr<value_type> GetImpl() {
     return current;
   }
 
-private:
-  Generator generator;
-  std::shared_ptr<T> current;
+  GeneratorType generator;
+  std::shared_ptr<value_type> current;
 };
 
-template <class T>
-class Get : public StreamProvider<T> 
+template <class ContainerType>
+class Container : public Iterator<typename ContainerType::iterator>
 {
 public:
-  Get(std::unique_ptr<StreamProvider<T>>&& source, std::size_t n):
-    source(std::move(source)), n(n) {}
+  using value_type = typename ContainerType::value_type;
 
-  virtual ~Get() = default;
-
-  bool advance() override {
-    if (current < n) {
-      ++current;
-      return source->advance();
-    }
-    return false;
-  }
-
-  std::shared_ptr<T> get() override {
-    return source->get();
+  Container(ContainerType&& container) :
+  container(std::move(container))
+  {
+    current = this->container.begin()
+    end = this->container.end();
   }
 
 private:
-  std::unique_ptr<StreamProvider<T>> source;
-  std::size_t n;
-  std::size_t current = 0;
+  ContainerType container;
 };
 
-template <class T, class Transform>
-class Map : public StreamProvider<std::result_of_t<Transform(T)>> 
+namespace traits {
+
+template <class Provider>
+struct is_finite
 {
-public:
-  Map(std::unique_ptr<StreamProvider<T>>&& source, Transform&& transform):
-    source(std::move(source)), transform(std::forward<Transform>(transform)) {}
-
-  virtual ~Map() = default;
-
-  bool advance() override {
-    return source->advance();
-  }
-
-  std::shared_ptr<std::result_of_t<Transform(T)>> get() override {
-    return std::make_shared<std::result_of_t<Transform(T)>>(transform(*source->get()));
-  }
-
-private:
-  std::unique_ptr<StreamProvider<T>> source;
-  Transform transform;
+  constexpr std::optional<bool> optvalue = std::nullopt;
 };
 
-template <class T, class Predicate>
-class Filter : public StreamProvider<T>
+template <class IteratorType>
+struct is_finite<Iterator<IteratorType>>
 {
-public:
-  Filter(std::unique_ptr<StreamProvider<T>>&& source, Predicate&& predicate):
-    source(std::move(source)), predicate(std::forward<Predicate>(predicate)) {}
-
-  virtual ~Filter() = default;
-
-  bool advance() override {
-    while (source->advance()) {
-      current = source->get();
-      if (predicate(*current))
-        return true;
-    }
-    current.reset();
-    return false;
-  }
-
-  std::shared_ptr<T> get() override {
-    return current;
-  }
-
-private:
-  std::unique_ptr<StreamProvider<T>> source;
-  std::shared_ptr<T> current;
-  Predicate predicate;
-  bool first = true;
+  constexpr std::optional<bool> optvalue = true;
 };
 
-template <class T>
-class Group : public StreamProvider<std::vector<T>>
+template <class GeneratorType>
+struct is_finite<Generator<GeneratorType>>
 {
-public:
-  Group(std::unique_ptr<StreamProvider<T>>&& source, size_t group_size) :
-    source(std::move(source)), group_size(group_size) {}
-
-  virtual ~Group() = default;
-
-  bool advance() override {
-    if (stream_ended) {
-      current.reset();
-      return false;
-    }
-    current = std::make_shared<std::vector<T>>();
-    for (size_t i = 0; i < group_size; ++i) {
-      if (source->advance())
-        current->emplace_back(std::move(*source->get()));
-      else {
-        stream_ended = true;
-        break;
-      }
-    }
-    return true;
-  }
-
-  std::shared_ptr<std::vector<T>> get() override {
-    return current;
-  }
-
-private:
-  std::unique_ptr<StreamProvider<T>> source;
-  std::shared_ptr<std::vector<T>> current;
-  size_t group_size;
-  bool stream_ended = false;
+  constexpr std::optional<bool> optvalue = false;
 };
+
+template <class ContainerType>
+struct is_finite<Container<ContainerType>>
+{
+  constexpr std::optional<bool> optvalue = true;
+}
+
+} // namespace traits
+} // namespace core
+
+namespace composite {
+
+namespace traits {
+
+} // namespace traits
+} // namespace composite
 
 } // namespace providers
-
-template <class T>
-using StreamProviderPtr = std::unique_ptr<providers::StreamProvider<T>>;
-
 } // namespace stream
-
-#endif
