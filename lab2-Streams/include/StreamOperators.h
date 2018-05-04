faa96483d621a8ad38e94c922169f45e43b8b570
Alexander Gordeev <agordeevw@gmail.com>
2018-05-04 07:35:35 +0700

    Implemented operators

--- lab2-Streams/include/StreamOperators.h
+++ lab2-Streams/include/StreamOperators.h
@@ -1,61 +1,122 @@
 #ifndef INCLUDE_STREAMOPERATORS_H
 #define INCLUDE_STREAMOPERATORS_H
 
-#include <stdexcept>
-#include <type_traits>
-#include <utility>
-
 #include "StreamProviders.h"
 
 namespace stream {
-  auto get(std::size_t n) {
-    return Operator([=](auto&& stream) mutable {
-      using T = typename std::remove_reference_t<decltype(stream)>::value_type;
-      return Stream<T>(std::move(
-        std::make_unique<providers::Get<T>>(
-          std::move(stream.GetSource()), n)));
-    });
-  }
-  
-  template <class Transform>
-  auto map(Transform&& transform) {
-    return Operator([=] (auto&& stream) mutable {
-      using T = typename std::remove_reference_t<decltype(stream)>::value_type;
-      using U = std::result_of_t<Transform(T)>;
-      return Stream<U>(std::move(
-        std::make_unique<providers::Map<T, Transform>>(
-          std::move(stream.GetSource()), std::forward<Transform>(transform))));
-    });
+namespace operators {
+
+class Get
+{
+public:
+  Get(size_t amount) : amount(amount) {}
+
+  template <class Provider>
+  auto operator()(Stream<Provider>&& stream) {
+    using provider_type = providers::composite::Get;
+    return Stream(provider_type(std::move(stream.GetProvider()),
+      amount
+    ));
   }
 
-  template <class Predicate>
-  auto filter(Predicate&& predicate) {
-    return Operator([=] (auto&& stream) mutable {
-      using T = typename std::remove_reference_t<decltype(stream)>::value_type;
-      return Stream<T>(std::move(
-        std::make_unique<providers::Filter<T, Predicate>>(
-          std::move(stream.GetSource()), std::forward<Predicate>(predicate))));
-    });
+private:
+  size_t amount;
+};
+
+class Skip
+{
+public:
+  Skip(size_t amount) : amount(amount) {}
+
+  template <class Provider>
+  auto operator()(Stream<Provider>&& stream) {
+    auto& provider = stream.GetProvider();
+    for (size_t i = 0; i < amount; ++i)
+      provider.Advance()
+    return Stream(std::move(stream.GetProvider()));
+  }
+};
+
+template <class Transform>
+class Map
+{
+public:
+  Map(Transform&& transform) : 
+  transform(std::forward<Transform>(transform))
+  {}
+
+  template <class Provider>
+  auto operator()(Stream<Provider>&& stream) {
+    using provider_type = providers::composite::Map;
+    return Stream(provider_type(std::move(stream.GetProvider()), 
+      std::move(transform)
+    ));
   }
 
-  auto skip(std::size_t amount) {
-    return Operator([=] (auto&& stream) mutable {
-      using T = typename std::remove_reference_t<decltype(stream)>::value_type;
-      auto& source = stream.GetSource();
-      for (size_t i = 0; i < amount; ++i)
-        source->advance();
-      return Stream<T>(std::move(source));
-    });
+private:
+  Transform transform;
+};
+
+template <class Predicate>
+class Filter
+{
+public:
+  Filter(Predicate&& Predicate) :
+  predicate(std::forward<Predicate>(predicate))
+  {}
+
+  template <class Provider> 
+  auto operator() (Stream<Provider>&& stream) {
+    using provider_type = providers::composite::Filter;
+    return Stream(provider_type(std::move(stream.GetProvider()),
+      std::move(predicate)
+    ));
   }
 
-  auto group(std::size_t N) {
-    return Operator([=] (auto&& stream) mutable {
-      using T = typename std::remove_reference_t<decltype(stream)>::value_type;
-      return Stream<std::vector<T>>(std::move(
-        std::make_unique<providers::Group<T>>(
-          std::move(stream.GetSource()), N)));
-    });
+private:
+  Predicate predicate;
+};
+
+class Group
+{
+public:
+  Group(size_t size) : size(size) {}
+
+  template <class Provider>
+  auto operator() (Stream<Provider>&& stream) {
+    using provider_type = providers::composite::Group;
+    return Stream(provider_type(std::move(stream.GetProvider()),
+      size
+    ));
   }
+
+private:
+  size_t size;
+};
+
+} // namespace operators
+
+auto get(size_t n) {
+  return Operator(operators::Get(n));
+}
+
+template <class Transform>
+auto map(Transform&& transform) {
+  return Operator(operators::Map(std::forward<Transform>(transform)));
+}
+
+template <class Predicate>
+auto filter(Predicate&& predicate) {
+  return Operator(operators::Predicate(std::forward<Predicate>(predicate)));
+}
+
+auto skip(size_t amount) {
+  return Operator(operators::Skip(amount));
+}
+
+auto group(size_t size) {
+  return Operator(operators::Group(size));
+}
 
 } // namespace stream
 
