7bbbe142585858933221cc7b2f3d0a06f3c55f80
Alexander Gordeev <agordeevw@gmail.com>
2018-05-06 03:13:46 +0700

    Refactoring

--- lab2-Streams/include/StreamInterface.h
+++ lab2-Streams/include/StreamInterface.h
@@ -22,31 +22,39 @@ auto reduce(Accumulator&& accum) {
 
 template <class IdentityFn, class Accumulator>
 auto reduce(IdentityFn&& identityFn, Accumulator&& accum) {
-  return Terminator(terminators::Reduce(
-    std::forward<IdentityFn>(identityFn),
-    std::forward<Accumulator>(accum)
-  ));
+  return Terminator(
+    terminators::Reduce(
+      std::forward<IdentityFn>(identityFn),
+      std::forward<Accumulator>(accum)
+    )
+  );
 }
 
 auto sum() {
-  return Terminator(terminators::Reduce(
-    [](auto x) { return x; },
-    [](auto x, auto y) { return x + y; }
-  ));
+  return Terminator(
+    terminators::Reduce(
+      [](auto x) { return x; },
+      [](auto x, auto y) { return x + y; }
+    )
+  );
 }
 
 auto print_to(std::ostream& os, const char* delimiter = " ") {
-  return Terminator(terminators::PrintTo(
-    os, delimiter
-  ));
+  return Terminator(
+    terminators::PrintTo(os, delimiter)
+  );
 }
 
 auto to_vector() {
-  return Terminator(terminators::ToVector());
+  return Terminator(
+    terminators::ToVector()
+  );
 }
 
 auto nth(size_t index) {
-  return Terminator(terminators::Nth(index));
+  return Terminator(
+    terminators::Nth(index)
+  );
 }
 
 // ---------------------------------------------------------
@@ -54,25 +62,39 @@ auto nth(size_t index) {
 // ---------------------------------------------------------
 
 auto get(size_t n) {
-  return Operator(operators::Get(n));
+  return Operator(
+    operators::Get(n)
+  );
 }
 
 template <class Transform>
 auto map(Transform&& transform) {
-  return Operator(operators::Map(std::forward<Transform>(transform)));
+  return Operator(
+    operators::Map(
+      std::forward<Transform>(transform)
+    )
+  );
 }
 
 template <class Predicate>
 auto filter(Predicate&& predicate) {
-  return Operator(operators::Filter(std::forward<Predicate>(predicate)));
+  return Operator(
+    operators::Filter(
+      std::forward<Predicate>(predicate)
+    )
+  );
 }
 
 auto skip(size_t amount) {
-  return Operator(operators::Skip(amount));
+  return Operator(
+    operators::Skip(amount)
+  );
 }
 
 auto group(size_t size) {
-  return Operator(operators::Group(size));
+  return Operator(
+    operators::Group(size)
+  );
 }
 
 } // namespace stream
